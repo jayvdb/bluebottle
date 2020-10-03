572e359b64c563a93d54007be7cf81458d6f4972
John Vandenberg <jayvdb@gmail.com>
2020-10-03 16:18:06 +0700

    de-jet

--- bluebottle/clients/context_processors.py
+++ bluebottle/clients/context_processors.py
@@ -1,7 +1,7 @@
 from bluebottle.cms.models import SitePlatformSettings
 from django.db import connection
 from bluebottle.clients import properties
-from bluebottle.looker.models import LookerEmbed
+#from bluebottle.looker.models import LookerEmbed
 
 
 def tenant(request):
@@ -13,7 +13,7 @@ def tenant(request):
         'properties': properties,
         'logo': site_settings.logo,
     }
-    if hasattr(request, 'user') and request.user.has_perm('looker.access_looker_embeds'):
-        context['looker_items'] = LookerEmbed.objects.all()
+    #if hasattr(request, 'user') and request.user.has_perm('looker.access_looker_embeds'):
+    #    context['looker_items'] = LookerEmbed.objects.all()
 
     return context
