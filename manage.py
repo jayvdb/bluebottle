69acc9f425d807bca8819aef61fb988192118f1e
John Vandenberg <jayvdb@gmail.com>
2020-10-03 16:14:19 +0700

    manage.py: django-compat-patcher

--- manage.py
+++ manage.py
@@ -2,8 +2,12 @@
 import os
 import sys
 
+import django_compat_patcher
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bluebottle.settings.local")
+
+    django_compat_patcher.patch()
 
     from django.core.management import execute_from_command_line
 
