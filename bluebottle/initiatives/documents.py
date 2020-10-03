4eee97c05fa5039217c58ff61fa94aa3764f1e2d
John Vandenberg <jayvdb@gmail.com>
2020-10-03 16:05:51 +0700

    Add 'Django'

--- bluebottle/initiatives/documents.py
+++ bluebottle/initiatives/documents.py
@@ -91,6 +91,8 @@ class InitiativeDocument(DocType):
             Geolocation, Member, ProjectTheme, Event, Funding, Assignment
         )
 
+    Django = Meta
+
     def get_queryset(self):
         return super(InitiativeDocument, self).get_queryset().select_related(
             'theme', 'place', 'owner', 'promoter',
