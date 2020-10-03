af6fec8d00653a694c7badf4da8510ac1f161c50
John Vandenberg <jayvdb@gmail.com>
2020-10-03 16:14:19 +0700

    manage.py: Use django-compat-patcher on Django 2+

--- manage.py
+++ manage.py
@@ -5,6 +5,12 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bluebottle.settings.local")
 
+    from django import VERSION
+    if VERSION >= (2, 0):
+        import django_compat_patcher
+
+        django_compat_patcher.patch()
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
